f7864336b35d5e3a69dd3c2d0891bbd9f7d85e66
AJ-1009 <ashwin_b210819ee@nitc.ac.in>
2022-12-07 01:12:51 +0530

    index news

--- pages/api/news.js
+++ pages/api/news.js
@@ -2,7 +2,6 @@
 
 export function indexnews(page){
     return new Promise ((resolve,reject)=>{
-        let data = fetch(`https://newsapi.org/v2/top-headlines?category?&language=en&apiKey=${process.env.NEXT_PUBLIC_API_KEY}&page=${page}`)
-        resolve(data)
+        fetch(`https://newsapi.org/v2/top-headlines?category?&language=en&apiKey=${process.env.NEXT_PUBLIC_API_KEY}&page=${page}`).then((res)=>res.json).then((res)=>resolve(res))
     })
 }
